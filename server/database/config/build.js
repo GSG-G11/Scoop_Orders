e0f601b941d5fe92a6adbe003d0ae6f91f52173c
MostafaQanoo <mostafaqanoo403@gmail.com>
2022-03-10 01:37:04 +0200

    deploy

--- server/database/config/build.js
+++ server/database/config/build.js
@@ -1,11 +1,22 @@
-const connection = require('../config/connection');
-const {readFileSync} = require('fs');
-const {join} = require('path') 
+// const connection = require('../config/connection');
+// const {readFileSync} = require('fs');
+// const {join} = require('path') 
 
+
+// const dbBuild = () => {
+//   const sql = readFileSync(join(__dirname, "build.sql")).toString();
+//   return connection.query(sql);
+// };
+
+// module.exports =  dbBuild ;
+
+const{join} = require('path');
+const {readFileSync} = require('fs');
+const connection = require('./connection');
 
 const dbBuild = () => {
-  const sql = readFileSync(join(__dirname, "build.sql")).toString();
-  return connection.query(sql);
-};
-
-module.exports =  dbBuild ;
+    const sql = readFileSync(join(__dirname, "data.sql")).toString();
+    return connection.query(sql);
+  };
+  
+  module.exports =  dbBuild ;
