78b1356dfb3ebb67cc97b0d57b91825e6232a6a6
MostafaQanoo <mostafaqanoo403@gmail.com>
2022-03-09 12:04:54 +0200

    create static database relates #7

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,18 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const addUser = require("../database/queries/postData");
-const getUsers = require("../database/queries/getData");
+const {getUsers,getMenu} = require("../database/queries/getData");
+
 
 router.post("/create-order", (req, res) => {
   addUser(req.body.name, req.body.location, req.body.phone).then((data) =>
     console.log(data.rows)
   );
-  res.redirect("/");
+  res.redirect("/menu.html");
 });
 router.get("/get-order", (req, res) => {
   getUsers()
-    .then((data) => console.log(data))
+    // .then((data) => console.log(data))
     .then((data) => res.json(data));
+});
+router.get("/get-menu", (req, res) => {
+  getMenu()
+    // .then((data) => console.log(data.rows))
+    .then((data) => res.json(data.rows));
 });
 
 module.exports = router;
